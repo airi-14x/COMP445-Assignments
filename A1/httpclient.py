12f341ccf452aa7d45a93bebd2158905e148fceb
Pandaromin <earlsteven.aromin@mail.concordia.ca>
2018-09-26 17:46:05 -0400

    Fixes indentation errors

--- A1/httpclient.py
+++ A1/httpclient.py
@@ -13,26 +13,26 @@ import sys
 
 def run_httpclient(host, port):
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	try:
 		sock.connect((host,port))
         # print("Request line? ")
 		while True:
 			#line = sys.stdin.readline(1024)
-            request = ("GET /status/418 HTTP/1.0\r\nHost: httpbin.org\r\n\r\n”)
-            request = request.encode("utf-8")
+			request = ("GET /status/418 HTTP/1.0\r\nHost: httpbin.org\r\n\r\n")
+			request = request.encode("utf-8")
             #request = line.encode("utf-8")
 			sock.sendall(request)
 
 			response = sock.recv(1024,socket.MSG_WAITALL)
-			response = response.decode(“utf-8”)
+			response = response.decode("utf-8")
 			sys.stdout.write(response)
 
 	finally:
-	sock.close()
+		sock.close()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--host", help="server host", default="localhost")
 parser.add_argument("--port", help="server port", type=int, default=8007)
 args = parser.parse_args()
-run_client(args.host, args.port)
+run_httpclient(args.host, args.port)
