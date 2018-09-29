18c491d54a20e65d5c23094c0d0926a2b3453504
airi-14x <airi.14x@gmail.com>
2018-09-29 18:20:10 -0400

    Getting the POST to work

--- A1/httpclient.py
+++ A1/httpclient.py
@@ -11,16 +11,25 @@ def run_httpclient(host, port):
         # print("Request line? ")
 		while True:
 			#line = sys.stdin.readline(1024)
-			request = ("GET /status/418 HTTP/1.0\r\nHost: httpbin.org\r\n\r\n")
+			#request = ("GET /status/418 HTTP/1.0\r\nHost: httpbin.org\r\n\r\n")
+			#request = ("POST /post HTTP/1.0\r\nHost: httpbin.org\r\nContent-Length:\r\nContent-Type:application/json\r\nsmol:duck\r\n\r\nhello=me\r\n\r\n")
+			request_line = ("POST /post HTTP/1.0\r\n")
+			header_lines=("Host:httpbin.org\r\nContent-Type:application/json\r\n")
+			body =("body=shown")
+			header_lines+=("Content-length:" + str(len(body)) + "\r\n\r\n")
+
+			full_request = (request_line+header_lines+body).encode("utf-8")
+
 			'''EXAMPLE POST REQUEST LINE
 				    request = ("POST /post HTTP/1.0\r\nHost: httpbin.org\r\nsmol:duck\r\n\r\n")
 					Header information can be sent.
 					Issue: sending body information
 			'''
-			request = request.encode("utf-8")
+			#request = request.encode("utf-8")
+			full_request = (request_line+header_lines+body).encode("utf-8")
             #request = line.encode("utf-8")
-			sock.sendall(request)
-
+			#sock.sendall(request)
+			sock.sendall(full_request)
 			response = sock.recv(1024,socket.MSG_WAITALL)
 			response = response.decode("utf-8")
 			sys.stdout.write(response)
