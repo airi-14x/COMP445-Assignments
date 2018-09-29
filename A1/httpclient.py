b57af33aceedf650907fc57ae8994a9da9850f97
Pandaromin <earlsteven.aromin@mail.concordia.ca>
2018-09-29 19:26:01 -0400

    Fixes broken pipe error
    Also adds example of get request with args

--- A1/httpclient.py
+++ A1/httpclient.py
@@ -5,30 +5,34 @@ import sys
 
 def run_httpclient(host, port):
 
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	try:
-		sock.connect((host,port))
         # print("Request line? ")
 		while True:
+			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+			sock.connect((host,port))
+
 			#line = sys.stdin.readline(1024)
 			#request = ("GET /status/418 HTTP/1.0\r\nHost: httpbin.org\r\n\r\n")
 			#request = ("POST /post HTTP/1.0\r\nHost: httpbin.org\r\nContent-Length:\r\nContent-Type:application/json\r\nsmol:duck\r\n\r\nhello=me\r\n\r\n")
+			#get req with args
+			#full_request  = ("GET /get?course=networking&assignment=1 HTTP/1.0\r\nHost: httpbin.org\r\n\r\n")
+			#full_request = full_request.encode("utf-8")
+
+		#		POST
 			request_line = ("POST /post HTTP/1.0\r\n")
 			header_lines=("Host:httpbin.org\r\nContent-Type:application/json\r\n")
 			body =("body=shown")
 			header_lines+=("Content-length:" + str(len(body)) + "\r\n\r\n")
-
+#
 			full_request = (request_line+header_lines+body).encode("utf-8")
 
-			'''EXAMPLE POST REQUEST LINE
-				    request = ("POST /post HTTP/1.0\r\nHost: httpbin.org\r\nsmol:duck\r\n\r\n")
-					Header information can be sent.
-					Issue: sending body information
-			'''
+			
 			#request = request.encode("utf-8")
-			full_request = (request_line+header_lines+body).encode("utf-8")
+
             #request = line.encode("utf-8")
 			#sock.sendall(request)
+			
 			sock.sendall(full_request)
 			response = sock.recv(1024,socket.MSG_WAITALL)
 			response = response.decode("utf-8")
