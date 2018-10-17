e5c1bacaea6e97f01a9649852b0afdecdb8879d2
airi-14x <airi.14x@gmail.com>
2018-10-17 18:56:04 -0400

    Update

--- A2/httpc-server.py
+++ A2/httpc-server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import argparse
+
 def run_server(host,port):
     listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
@@ -13,11 +14,26 @@ def run_server(host,port):
     finally:
         listener.close()
 
-
 def handle_client(conn,addr):
     print(addr, " has connected.")
-
+    try:
+        data = conn.recv(1024)
+        print(data.decode('utf-8'))
+        #I think our response will be completely different from the request. It's just resent in the example since it's echoserver
+        #yep~ for now, we can do this
+        #We'll be decoding, modifying the message with a response and sending it back?
+        #right, so for our request, we would have to get whatever the request and write the correct
+        # response. 
+        
+        #That's fine haha
+        dummy_response = "fake status line\r\nfake header \r\n\r\n body: request ack"
+        conn.sendall(dummy_response.encode('utf-8'))
+        #how does this look?, tried to keep the http message format but it's nice to know if we can send stuff back
+    finally:
+        conn.close()
+    
 parser = argparse.ArgumentParser()
 parser.add_argument("--port", help="echo server port", type=int, default=8007)
 args = parser.parse_args()
 run_server('', args.port)
+
