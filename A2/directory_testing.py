94a7f587832622a277dcebc0b68db7692a6ee09b
Pandaromin <earlsteven.aromin@mail.concordia.ca>
2018-10-22 16:52:30 -0400

    Added cwd get

--- A2/directory_testing.py
+++ A2/directory_testing.py
@@ -7,11 +7,12 @@ def check_dir(path):
 	if path is not None:
 		current_dir = os.listdir(path)
 	else:
-		current_dir = os.listdir()
+		current_dir = os.listdir(os.getcwd())
 	for filename in current_dir:
 		if(os.path.isdir(filename)):
 			filename = "[Folder]" + filename
 		print(filename)
+
 	pass
 
 check_dir(None)
