a839f50e0824d408d6dd13241feea5c91ba85ccc
airi-14x <airi.14x@gmail.com>
2018-10-26 17:59:06 -0400

    Working File Server App

--- A2/httpc-server.py
+++ A2/httpc-server.py
@@ -10,6 +10,7 @@ current_dir = os.getcwd()
 response_body = ""
 status_line = "HTTP/1.0 "
 status_codes = {200 : "200 OK \r\n",
+                201 : "201 Created \r\n",
                         400: "400 Bad Request \r\n",
                         404: "404 Not Found\r\n"}
 def run_server(host,port):
@@ -36,13 +37,17 @@ def handle_client(conn,addr):
         print(request_text) 
         
         # Parsing the Request #
-        request_contents = request_text.split("\r\n\r\n")
-        header_contents = request_contents[0].split()
+        request_content = request_text.split("\r\n\r\n")
+        header_contents = request_content[0].split()
         request_method = header_contents[0]
         request_path = header_contents[1]
+        
+        request_body = None
+        #only set request_body if a body exists
+        if len(request_content)>1: 
+            request_body = request_content[1]
+        
         # Check whether GET/POST #
-        
-        
         if request_method == "GET":
             if len(request_path) == 1:
                 show_directory()
@@ -51,9 +56,9 @@ def handle_client(conn,addr):
                 print("GET/foo")
                 return_content(request_path)
                 #call GET/foo
-        elif request_method == "POST":
+        elif request_method == "POST" and request_body != None:
             print("POST /bar")
-            #call POST/bar
+            write_file(request_path, request_body)
         else:
             print("Bad Req")
             status_line += status_code[400]
@@ -104,15 +109,44 @@ def return_content(request_path):
     global status_codes
     global current_dir
     request_path = current_dir+request_path
+    # for foo.txt 
+    #request_path = "subFolderA/foo.txt"
     if(os.path.isfile(request_path)):
         current_file = open(request_path,"r")
         response_body = current_file.read()
-        print(response_body)
+        #print(response_body)
         current_file.close()
         status_line += status_codes[200]
     else:
         status_line += status_codes[404]
-     
+        
+def write_file(request_path,request_data):
+    global response_body
+    global status_line
+    global status_codes
+    global current_dir
+    path_split = request_path.split("/")
+    test_dir = ""
+    for x in range(len(path_split)-1):
+        test_dir += path_split[x] + "/"
+
+    test_dir = current_dir + test_dir
+    if os.path.isdir(test_dir):        
+        request_path = current_dir+request_path   
+        current_file = open(request_path,"w")
+        current_file.write(request_data)
+        current_file.close()
+        response_body += path_split[-1] + " created.\r\n"
+        response_body += "Size : " + str(os.path.getsize(request_path))
+        response_body += "\r\nContent: {\r\n"
+        current_file = open(request_path,"r")
+        response_body += current_file.read()
+        current_file.close()
+        response_body += "\r\n}\r\n"
+        status_line += status_codes[201]
+    else:
+        status_line += status_codes[400]
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--port", help="echo server port", type=int, default=8007)
 parser.add_argument("-v", help="printing debugging message", action="store_true")
@@ -120,3 +154,4 @@ parser.add_argument("-d", help="directory path", type=str, default=current_dir)
 args = parser.parse_args()
 current_dir = args.d
 run_server('', args.port)
+
