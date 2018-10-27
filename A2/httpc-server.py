e9c89ff22354b3dc1cb9d3b91f598b58597e80b7
Pandaromin <earlsteven.aromin@mail.concordia.ca>
2018-10-27 15:37:22 -0400

    Exceptions added

--- A2/httpc-server.py
+++ A2/httpc-server.py
@@ -12,6 +12,7 @@ status_line = "HTTP/1.0 "
 status_codes = {200 : "200 OK \r\n",
                 201 : "201 Created \r\n",
                         400: "400 Bad Request \r\n",
+                        403 : "403 Forbidden\r\n",
                         404: "404 Not Found\r\n"}
 def run_server(host,port):
     listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -66,14 +67,8 @@ def handle_client(conn,addr):
         
         #  Call GET / POST Functions #
         
-        #
-        #   Parsing and function calls end
-        # Yep. We need to be able to tell if it's a 404 or a 200 and stuff like that. Those require going through the functions we made
-        
         # Response Message #
         
-        #status_line += status_codes[200]   # Need to set the status code 'n' value #  
-   
         general_headers = "Date: "
         current_time = strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " GMT\r\n"
         general_headers += current_time
@@ -84,12 +79,10 @@ def handle_client(conn,addr):
        
         entity_headers = "Content-Type: text \r\nContent-Length: " + str(body_length) + "\r\n\r\n"
         response = status_line + general_headers + response_headers + entity_headers + response_body
-        #dummy_response = "fake status line\r\nfake header \r\n\r\n body: request ack"
-        #conn.sendall(dummy_response.encode('utf-8'))
+        
         conn.sendall(response.encode('utf-8'))
         response_body = ""
         status_line = "HTTP/1.0 "
-        #how does this look?, tried to keep the http message format but it's nice to know if we can send stuff back
     finally:
         conn.close()
 
@@ -103,22 +96,32 @@ def show_directory():
         #if(file.endswith(".txt")): #example of having the request header "Accept : .txt"
             response_body+=( os.path.join(subdir, file)) + "\n"
     status_line += status_codes[200]
+    
 def return_content(request_path):
     global response_body
     global status_line
     global status_codes
     global current_dir
     request_path = current_dir+request_path
-    # for foo.txt 
-    #request_path = "subFolderA/foo.txt"
-    if(os.path.isfile(request_path)):
-        current_file = open(request_path,"r")
+    
+    try:
+        current_file = open(request_path, "r")
         response_body = current_file.read()
-        #print(response_body)
         current_file.close()
         status_line += status_codes[200]
-    else:
+    except IOError:
+        print ("Error: File cannot be read")
         status_line += status_codes[404]
+        
+        
+    #if(os.path.isfile(request_path)):
+    #    current_file = open(request_path,"r")
+    #   response_body = current_file.read()
+        #print(response_body)
+    #    current_file.close()
+    #    status_line += status_codes[200]
+    #else:
+    #    status_line += status_codes[404]
         
 def write_file(request_path,request_data):
     global response_body
@@ -131,7 +134,8 @@ def write_file(request_path,request_data):
         test_dir += path_split[x] + "/"
 
     test_dir = current_dir + test_dir
-    if os.path.isdir(test_dir):        
+    
+    try:
         request_path = current_dir+request_path   
         current_file = open(request_path,"w")
         current_file.write(request_data)
@@ -144,8 +148,26 @@ def write_file(request_path,request_data):
         current_file.close()
         response_body += "\r\n}\r\n"
         status_line += status_codes[201]
-    else:
+    except IOError:
+        print("Error: Bad Request, Unrecognized Directory")
         status_line += status_codes[400]
+    
+    
+    #if os.path.isdir(test_dir):        
+    #    request_path = current_dir+request_path   
+    #    current_file = open(request_path,"w")
+    #    current_file.write(request_data)
+    #    current_file.close()
+    #    response_body += path_split[-1] + " created.\r\n"
+    #    response_body += "Size : " + str(os.path.getsize(request_path))
+    #    response_body += "\r\nContent: {\r\n"
+    #    current_file = open(request_path,"r")
+    #    response_body += current_file.read()
+        #current_file.close()
+       # response_body += "\r\n}\r\n"
+      #  status_line += status_codes[201]
+   # else:
+     #   status_line += status_codes[400]
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--port", help="echo server port", type=int, default=8007)
@@ -154,4 +176,3 @@ parser.add_argument("-d", help="directory path", type=str, default=current_dir)
 args = parser.parse_args()
 current_dir = args.d
 run_server('', args.port)
-
