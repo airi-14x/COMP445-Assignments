f2761c4ba829164835d0e04c148cb6e70bb4e128
Pandaromin <earlsteven.aromin@mail.concordia.ca>
2018-10-27 16:54:05 -0400

    Adds security checking, -p and -d

--- A2/httpc-server.py
+++ A2/httpc-server.py
@@ -14,6 +14,9 @@ status_codes = {200 : "200 OK \r\n",
                         400: "400 Bad Request \r\n",
                         403 : "403 Forbidden\r\n",
                         404: "404 Not Found\r\n"}
+
+debug_body = ""
+
 def run_server(host,port):
     listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
@@ -30,6 +33,7 @@ def handle_client(conn,addr):
     global response_body
     global status_line
     global status_codes
+    global debug_body
     print(addr, " has connected.")
     try:
         data = conn.recv(1024)
@@ -83,6 +87,9 @@ def handle_client(conn,addr):
         conn.sendall(response.encode('utf-8'))
         response_body = ""
         status_line = "HTTP/1.0 "
+        if args.v :
+            print(debug_body)
+        debug_body = ""
     finally:
         conn.close()
 
@@ -90,52 +97,65 @@ def show_directory():
     global response_body
     global status_line
     global status_codes
+    global debug_body
+    
+    debug_body += "Currently at show_directory()\r\n"
     rootdir = os.getcwd()
     for subdir, dirs, files in os.walk(rootdir):
         for file in files:
         #if(file.endswith(".txt")): #example of having the request header "Accept : .txt"
             response_body+=( os.path.join(subdir, file)) + "\n"
     status_line += status_codes[200]
+    debug_body += "Exiting show_directory with 200 OK\r\n"
     
 def return_content(request_path):
     global response_body
     global status_line
     global status_codes
     global current_dir
+    global debug_body
     request_path = current_dir+request_path
     
+    debug_body += "Currently at return_content()" + "\n"
     try:
+        if not(request_path.startswith(current_dir)):
+            raise ValueError('Forbidden Directory')
+
         current_file = open(request_path, "r")
         response_body = current_file.read()
         current_file.close()
         status_line += status_codes[200]
+        debug_body += "Exiting return_content() with 200 OK\r\n"
+    except ValueError:
+        status_line += status_codes[403]
+        debug_body += "Exiting return_content() with 403\r\n"
+        debug_body += "Permission Denied: outside of working dir \r\n"
     except IOError:
         print ("Error: File cannot be read")
         status_line += status_codes[404]
+        debug_body += "Exiting return_content() with 404 Not Found\r\n"
         
-        
-    #if(os.path.isfile(request_path)):
-    #    current_file = open(request_path,"r")
-    #   response_body = current_file.read()
-        #print(response_body)
-    #    current_file.close()
-    #    status_line += status_codes[200]
-    #else:
-    #    status_line += status_codes[404]
-        
+       
 def write_file(request_path,request_data):
     global response_body
     global status_line
     global status_codes
     global current_dir
+    global debug_body
+    
+    debug_body += "Currently at write_file() \r\n"
     path_split = request_path.split("/")
     test_dir = ""
     for x in range(len(path_split)-1):
         test_dir += path_split[x] + "/"
 
     test_dir = current_dir + test_dir
-    
     try:
+        #current defined as working directory, anything outside of it is off-limits
+        if not(test_dir.startswith(current_dir)):
+            raise ValueError('Forbidden Directory')
+        
+   
         request_path = current_dir+request_path   
         current_file = open(request_path,"w")
         current_file.write(request_data)
@@ -148,31 +168,20 @@ def write_file(request_path,request_data):
         current_file.close()
         response_body += "\r\n}\r\n"
         status_line += status_codes[201]
+        debug_body += "File creation success\r\n"
+        debug_body += "Exiting write_file() with 201 Creation Success\r\n"
+    except ValueError:
+        status_line += status_codes[403]
+        debug_body += "Exiting write_file() with 403\r\n"
+        debug_body += "Permission Denied: outside of working dir \r\n"
     except IOError:
-        print("Error: Bad Request, Unrecognized Directory")
+        debug_body += "Exiting write_file() with 400\r\nDirectory does not exist\r\n"
         status_line += status_codes[400]
-    
-    
-    #if os.path.isdir(test_dir):        
-    #    request_path = current_dir+request_path   
-    #    current_file = open(request_path,"w")
-    #    current_file.write(request_data)
-    #    current_file.close()
-    #    response_body += path_split[-1] + " created.\r\n"
-    #    response_body += "Size : " + str(os.path.getsize(request_path))
-    #    response_body += "\r\nContent: {\r\n"
-    #    current_file = open(request_path,"r")
-    #    response_body += current_file.read()
-        #current_file.close()
-       # response_body += "\r\n}\r\n"
-      #  status_line += status_codes[201]
-   # else:
-     #   status_line += status_codes[400]
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--port", help="echo server port", type=int, default=8007)
+parser.add_argument("-p", help="echo server port", type=int, default=8007)
 parser.add_argument("-v", help="printing debugging message", action="store_true")
 parser.add_argument("-d", help="directory path", type=str, default=current_dir)
-args = parser.parse_args()
-current_dir = args.d
-run_server('', args.port)
+args = parser.parse_args() 
+current_dir = os.getcwd() +  args.d
+run_server('', args.p) 
