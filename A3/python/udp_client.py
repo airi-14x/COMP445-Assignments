5115092991737d82ce12ad2f48f667cc2f207346
airi-14x <airi.14x@gmail.com>
2018-11-12 20:35:11 -0500

    Update

--- A3/python/udp_client.py
+++ A3/python/udp_client.py
@@ -2,6 +2,7 @@ import argparse
 import ipaddress
 import socket
 import random
+import packet
 
 from packet import Packet
 
@@ -22,61 +23,55 @@ def run_client(router_addr, router_port, server_addr, server_port):
         #conn.sendto(p.to_bytes(), (router_addr, router_port))
         #print('Send "{}" to router'.format(msg))
         
-        
         # Sending SYN #
-        send_SYN(conn,router_addr,router_port)
+        send_SYN(conn,router_addr,router_port,peer_ip,server_port)
+        
         # Receiving SYN-ACK #
         conn.settimeout(timeout)
         print('Waiting for SYN-ACK')
         expected_type = -1
         expected_seq = -1
-        while(expected_type!=packet.SYN_ACK or expected_seq != (seq_num + 1)):
+        while(expected_type!=packet.SYN_ACK):
             response,sender = conn.recvfrom(1024)
             recv_pkt = Packet.from_bytes(response)
             expected_type = recv_pkt.packet_type
-
-        ## Need to check out the server version of UDP Client to send the ACK
-        ## Need to fix the Send ACK with the expected_seq
+            print('Payload for expected SYN_ACK: ' + recv_pkt.payload.decode("utf-8"))
+        
+        send_ACK(conn,recv_pkt.peer_ip_addr,recv_pkt.port,recv_pkt.seq_num,peer_ip,server_port)
         ## Test the handshake ==> Resending packet version and the default version
-      
-        # Sending ACK #
-        packet_type = packet.ACK
-        #seq_num = -1 # Need to get ACK number from Server
-        
-        p = Packet(packet_type=packet_type,
-                   seq_num=seq_num,
-                   peer_ip_addr=peer_ip,
-                   peer_port=server_port,
-                   payload=msg.encode("utf-8"))
-        
-            
-        # Try to receive a response within timeout
-        
-        print('Waiting for a response')
-        response, sender = conn.recvfrom(1024)
-        p = Packet.from_bytes(response)
-        print('Router: ', sender)
-        print('Packet: ', p)
-        print('Payload: ' + p.payload.decode("utf-8"))
-
+               
     except socket.timeout:
         print('No response after {}s'.format(timeout))
     finally:
         conn.close()
 
-def send_SYN(conn, router_addr, router_port):
+def send_SYN(conn, router_addr, router_port,peer_ip,server_port):
     print('Initiating three-way handshake\nSending SYN')
     packet_type = packet.SYN
     seq_num = random.randint(1,50)
+    msg = "Initiating three-way handshake\nSending SYN"
     p = Packet(packet_type=packet_type,
                    seq_num=seq_num,
                    peer_ip_addr=peer_ip,
                    peer_port=server_port,
                    payload=msg.encode("utf-8"))
     conn.sendto(p.to_bytes(), (router_addr, router_port))
-# Usage:
-# python echoclient.py --routerhost localhost --routerport 3000 --serverhost localhost --serverport 8007
 
+def send_ACK(conn, router_addr, router_port, seq_number,peer_ip,server_port):
+    new_packet_type = packet.ACK  
+    new_peer_ip_addr = peer_ip  
+    new_peer_port = server_port
+    new_seq_num = seq_num + 1 
+    print ("Sending ACK")
+    msg = "Sending ACK"
+    p = Packet(packet_type=new_packet_type,
+                   seq_num=new_seq_num,
+                   peer_ip_addr=new_peer_ip_addr,
+                   peer_port=new_peer_port,
+                   payload=msg.encode("utf-8")) 
+    conn.sendto(p.to_bytes(),(router_addr, router_port))
+# Usage:
+# python echoclient.py --routerhost localhost --routerport 3000 --serverhost localhost --serverport 8007    
 parser = argparse.ArgumentParser()
 parser.add_argument("--routerhost", help="router host", default="localhost")
 parser.add_argument("--routerport", help="router port", type=int, default=3000)
@@ -86,3 +81,4 @@ parser.add_argument("--serverport", help="server port", type=int, default=8007)
 args = parser.parse_args()
 
 run_client(args.routerhost, args.routerport, args.serverhost, args.serverport)
+
--- A3/python/udp_server.py
+++ A3/python/udp_server.py
@@ -1,9 +1,11 @@
 import argparse
 import socket
+import random
+import packet
 
 from packet import Packet
 
-
+new_seq_num = 0
 def run_server(port):
     conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
@@ -18,16 +20,31 @@ def run_server(port):
 
 
 def handle_client(conn, data, sender):
+    global new_seq_num
     try:
         p = Packet.from_bytes(data)
         print("Router: ", sender)
         print("Packet: ", p)
         print("Payload: ", p.payload.decode("utf-8"))
-
-        # How to send a reply.
-        # The peer address of the packet p is the address of the client already.
-        # We will send the same payload of p. Thus we can re-use either `data` or `p`.
-        conn.sendto(p.to_bytes(), sender)
+        if(expected_type == packet.SYN):
+            new_seq_num - random.randint(1,50) # Can be any number. For last packet, need to
+                                           # check for +1 of this value.
+            new_packet_type = p.SYN_ACK  
+            new_peer_ip_addr = p.peer_ip_addr  
+            new_peer_port = p.peer_port
+            expected_type = p.packet_type
+                    
+            msg = "Sending SYN_ACK"
+            new_packet = Packet(packet_type = new_packet_type,
+                                seq_num = new_seq_num,
+                                peer_ip_addr=new_peer_ip_addr,
+                                peer_port=new_peer_port,
+                                payload =msg.encode("utf-8"))
+            conn.sendto(new_packet.to_bytes(), sender)
+       
+        elif(expected_type == ACK): 
+            if(new_seq_num == new_seq_num + 1):
+                print("Connection estblished")
 
     except Exception as e:
         print("Error: ", e)
@@ -38,3 +55,4 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--port", help="echo server port", type=int, default=8007)
 args = parser.parse_args()
 run_server(args.port)
+
